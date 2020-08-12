9b6f24f2ed011a705a10b32ae281b56d1865edf4
madhuvayya <madhu.vayya@gmail.com>
2020-08-12 13:48:28 +0530

    Added query to find the candidate name who did not submited bank details

--- queries.sql
+++ queries.sql
@@ -14,3 +14,11 @@ FROM lms.fellowship_candidate t1
     LEFT JOIN lms.candidate_documents t2
 		ON t1.candidate_id = t2.candidate_id
 WHERE t2.status IS NULL OR t2.status = 'Pending';
+
+-- query to find name of candidate who is not submit bank details
+
+SELECT t1.first_name
+FROM lms.fellowship_candidate t1
+    LEFT JOIN lms.candidate_bank_details t2
+		ON t1.candidate_id = t2.candidate_id
+WHERE t2.candidate_id IS NULL;
