e8668ec5fb8f82438849fe7a8dab9894ab95e732
madhuvayya <madhu.vayya@gmail.com>
2020-08-12 13:21:08 +0530

    Added query to find candidate name who did not submited documents

--- queries.sql
+++ queries.sql
@@ -6,3 +6,11 @@ FROM lms.fellowship_candidate t1
     LEFT JOIN lms.candidate_stack_assignment t2
 		ON t1.candidate_id = t2.candidate_id
 WHERE t2.candidate_id IS NULL;
+
+-- query find name of cnadidate which is not submit documents 
+
+SELECT t1.first_name
+FROM lms.fellowship_candidate t1
+    LEFT JOIN lms.candidate_documents t2
+		ON t1.candidate_id = t2.candidate_id
+WHERE t2.status = 'Pending';
